92c6ca513f0bc7cb4df40729d552b62a007b05dc
elbertgong <elbertgong@college.harvard.edu>
2019-01-20 15:44:55 -0500

    debug off

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SESSION_FILE_DIR'] = mkdtemp()
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
-app.debug=True # added
+# app.debug=True # added
 Session(app)
 
 @app.route('/')
